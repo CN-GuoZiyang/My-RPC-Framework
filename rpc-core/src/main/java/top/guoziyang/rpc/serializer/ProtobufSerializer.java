64b489f3c8d4453826028abd2a4f869e18035b01
tang-world <1399314573@qq.com>
2021-11-15 10:58:00 +0800

    Protostuff serializer

--- rpc-core/src/main/java/top/guoziyang/rpc/serializer/ProtobufSerializer.java
+++ rpc-core/src/main/java/top/guoziyang/rpc/serializer/ProtobufSerializer.java
@@ -16,7 +16,9 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ProtobufSerializer implements CommonSerializer {
 
+    // 防止频繁申请内存,存储序列化后的值,自动扩容
     private LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+    // 存储class对应的schema,会用到反射存储类信息,比较耗时间,所以存起来
     private Map<Class<?>, Schema<?>> schemaCache = new ConcurrentHashMap<>();
 
     @Override
@@ -49,9 +51,10 @@ public class ProtobufSerializer implements CommonSerializer {
         Schema schema = schemaCache.get(clazz);
         if (Objects.isNull(schema)) {
             // 这个schema通过RuntimeSchema进行懒创建并缓存
-            // 所以可以一直调用RuntimeSchema.getSchema(),这个方法是线程安全的
+            // 所以可以一直调用RuntimeSchema.getSchema(),该方法是线程安全的
             schema = RuntimeSchema.getSchema(clazz);
             if (Objects.nonNull(schema)) {
+                // key值一样会被替换,所以没有线程安全问题
                 schemaCache.put(clazz, schema);
             }
         }
