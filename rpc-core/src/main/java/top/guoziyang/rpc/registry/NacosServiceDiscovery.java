857ac88d98f9ffaf08b5795b3dfe17d8365a52c5
chenzhao <chenzhao@egova.com.cn>
2021-12-29 01:09:41 +0800

    reform code

--- rpc-core/src/main/java/top/guoziyang/rpc/registry/NacosServiceDiscovery.java
+++ rpc-core/src/main/java/top/guoziyang/rpc/registry/NacosServiceDiscovery.java
@@ -23,7 +23,7 @@ public class NacosServiceDiscovery implements ServiceDiscovery {
     private final LoadBalancer loadBalancer;
 
     public NacosServiceDiscovery(LoadBalancer loadBalancer) {
-        if(loadBalancer == null) this.loadBalancer = new RandomLoadBalancer();
+        if (loadBalancer == null) this.loadBalancer = new RandomLoadBalancer();
         else this.loadBalancer = loadBalancer;
     }
 
@@ -31,7 +31,7 @@ public class NacosServiceDiscovery implements ServiceDiscovery {
     public InetSocketAddress lookupService(String serviceName) {
         try {
             List<Instance> instances = NacosUtil.getAllInstance(serviceName);
-            if(instances.size() == 0) {
+            if (instances.size() == 0) {
                 logger.error("找不到对应的服务: " + serviceName);
                 throw new RpcException(RpcError.SERVICE_NOT_FOUND);
             }
